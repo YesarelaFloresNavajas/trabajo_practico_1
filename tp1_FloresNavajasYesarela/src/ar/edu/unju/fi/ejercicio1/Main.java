ac35f7a2b3af864af56280cc48c2a148deb946af
Yesarela Flores Navajas <yesarelafloresnavajas@gmail.com>
2024-04-12 18:34:38 -0300

    resolución del ejercicio 1

---
+++ tp1_FloresNavajasYesarela/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1;
+		int var2;
+		int var3;
+		int var4;
+		int var5;
+		float promedio;
+		
+		var1 = 2;
+		var2 = 4;
+		var3 = 7;
+		var4 = 9;
+		var5 = 5;
+		float valorTotal = var1 + var2 + var3 + var4 + var5 ;
+		
+		promedio = valorTotal / 5;
+		
+		System.out.println("El promedio es: " + promedio);
+	}
+
+}
