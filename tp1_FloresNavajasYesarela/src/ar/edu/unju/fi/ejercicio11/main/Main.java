e4fbc75358d565bf4a5e8afb1de05e32570c274c
Yesarela Flores Navajas <yesarelafloresnavajas@gmail.com>
2024-04-20 21:27:30 -0300

    resolución ejercicio 11

---
+++ tp1_FloresNavajasYesarela/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for(int i = 2, j = 40; j > 0; i++, j--) {
+			int serie = i * j;
+			System.out.print(serie + " ");		
+		}
+
+	}
+
+}
